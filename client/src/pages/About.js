0e6b529470199f911a1795cc60a19c050d72f21d
John Carlo <carlobase@gmail.com>
2020-04-28 17:54:56 -0500

    initiated front end

---
+++ client/src/pages/About.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+
+function About() {
+  return (
+    <div>
+      <Hero backgroundImage="https://images.pexels.com/photos/1283219/pexels-photo-1283219.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
+        <h1>Drinks on Us!</h1>
+        <h2>"When I read about the evils of drinking I gave up reading."</h2>
+      </Hero>
+      <Container style={{ marginTop: 30 }}>
+        <Row>
+          <Col size="md-12">
+            <h1>Welcome To Drinks on Us!!</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+            <p>
+            Q: What is a man's idea of a balanced diet? A: A Budweiser in each hand! Q: Whats the difference between Amy Winehouse and Captain Morgan? A: Captain Morgan comes alive when you add coke! Q: What did the bartender say after Charles Dickens ordered a martini? A: "Olive or twist?" Q: What did the bartender say after a book walked into the bar? A: "Please, no stories!" Q. Why did God invent Jameson whiskey? A. So the Irish would never rule the world! Q: What do Russians get when mixing Holy Water with Vodka? A: The Holy Spirit! Q: What goes with Coronavirus? A: Lyme disease. Q: What did the man with slab of asphalt under his arm order? A: "A beer please, and one for the road." Q: You know what's fun about being sober? A: Nothing.
+
+
+            </p>
+            <p>
+            Q: What has eight arms and an IQ of 60? A: Four guys drinking Bud Light and watching a football game! Q: How can you tell if someone Amish is an alcoholic? A: They keep falling off the wagon. Q: How many men does it take to open a Budweiser bottle? A: none. the lady should already have it open on the table! Q: How can you find the guy who drank a case of Coors Light? A: He's the one dancing like an asshole! Q: How do you know a man is really really gay? A: When he's nursing a Bacardi Breezer! Q: What happens when a ghost drinks boos? A: They get sheet-faced. Q: What's the difference between a G-Spot and a bottle of Jack Daniels? A: A guy will actually SEARCH for a bottle of Jack Daniels. Q: How does a man show he's planning for the future? A: He buys two cases of Miller Lite instead of one. What's the cure for marriage? Answer: Alcoholism. Q: Why does Corona go through your system so fast? A: Because it does not have to stop to change color Q: How do you get a computer drunk? A: A Screenshot of Tequila. Q: When do women drink alcohol? A: Wine O'Clock.
+
+
+            </p>
+            <p>
+            Boy: "I love you so much, I could never live without you." Girl: "Is that you or the beer talking?" Boy: "It's me talking to the beer." Mayan: Hey wanna drink? Other Mayan: I'm working on this calendar, but I guess if I don't finish it won't be the end of the world. Alcohol should be served in Capri Sun pouches. When you can't get the straw in the hole you've had enough. Why did the girl blow chunks all over the house? She wasn't party trained. Hear about the wall that went out on the town for its birthday?... Got plastered... Alcohol doesn't turn people into somebody they're not. It just makes them forget to hide that part of themselves. Life and beer are very similar .....chill for best results. I'm not an alcoholic. Alcoholics need a drink, but I already have one. Beauty is in the eye of the beer holder. If you drink too much alcohol you are an alcoholic. If you drink too much Fanta, does that make you Fantastic? I don't recycle because it makes me look like a huge alcoholic to my garbage man. I'm not an alcoholic alcoholics go to meetings, I'm a drunk, we go to parties. I spilled a drink on this girl at a party and that's "How I Wet Your Mother".
+
+
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default About;
