cbaf6e75d99388026fc0c69df455726f90fc2fdd
Evan Morisato <evanmorisato@yahoo.com>
2020-05-12 20:50:40 -0500

    Saved page is wokring. Deletes saved drinks from saved page. Still need to work on ingredient function

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,33 +1,36 @@
 import React, { Component } from "react";
-//import Jumbotron from "../components/Jumbotron"
-// import API from "../Utils/API"
-import DrinkListItem from "../components/DrinkListItem"
+import Jumbotron from "../components/Jumbotron"
+import API from "../Utils/API"
+// import DrinkListItem from "../components/DrinkListItem"
 import {DrinkList} from "../components/DrinkList"
+import Results from "../components/Results"
 
 class Saved extends Component {
     state = {
         savedDrinks: []
     }
 
-    // componentDidMount() {
-    //     API.savedDrinks()
-    //         .then(savedDrinks => this.setState({ savedDrinks: savedDrinks }))
-    //         .catch(err => console.error(err));
-    // }
+    componentDidMount() {
+        API.savedDrinks()
+            .then(savedDrinks => this.setState({ savedDrinks: savedDrinks }))
+            .catch(err => console.error(err));
+    }
 
     render() {
         return (
             <div>
+                <Jumbotron/>
             {this.state.savedDrinks.length ? (
                 <DrinkList>
-                {this.state.savedDrinks.map(filter => {
-                  console.log(filter);
+                {this.state.savedDrinks.map(drink => {
+                  console.log(drink);
                   return (
-                    <DrinkListItem
-                      strDrink={filter.strDrink}
-                      idDrink={filter.idDrink}
-                      strDrinkThumb={filter.strDrinkThumb}
-                      />
+                    <Results
+                        drinks={drink}
+                        strDrink={drink.drink}
+                        idDrink={drink.id}
+                        strDrinkThumb={drink.image}
+                        />
                       );
                     })}
               </DrinkList>
